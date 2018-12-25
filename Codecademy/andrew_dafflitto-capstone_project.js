b2962586da1ca4042f9212ccd1118b8b5457b6a5
Andrew M Dafflitto <31359912+adafflitto@users.noreply.github.com>
2018-12-25 13:58:00 -0600

    Update andrew_dafflitto-capstone_project.js

--- Codecademy/andrew_dafflitto-capstone_project.js
+++ Codecademy/andrew_dafflitto-capstone_project.js
@@ -10,102 +10,101 @@ _.clamp = (num, min, max) => {
 
 
 _.inRange = (number, start, end) => {
-  if (end === undefined) {
-    end = start;
-    start = 0;
-  };
-  if (start > end) {
-    let temp = start;
-    start = end;
-    end = temp;
-  };
-  let isInRange = (number >= start && number <= end);
-  return isInRange;
+	if (end === undefined) {
+		end = start;
+		start = 0;
+	};
+	if (start > end) {
+        	let temp = start;
+        	start = end;
+        	end = temp;
+        };
+        let isInRange = (number >= start && number <= end);
+        return isInRange;
 };
 
 
 
 _.words = string => {
-  let arr = string.split(' ');
-  return arr;
+	return string.split(' ');
 };
 
 
 
 _.pad = (string, length) => {
-  if (string.length > length) {
-    return string;
-  }
-  let begpadding = Math.floor((length - string.length) / 2);
-  let endpadding = length - string.length - begpadding;
-  let paddedString = ' '.repeat(begpadding) + string + ' '.repeat(endpadding);
-  return paddedString;
+	if (string.length > length) {
+    	return string;
+    }
+    let begpadding = Math.floor((length - string.length) / 2);
+    let endpadding = length - string.length - begpadding;
+    let paddedString = ' '.repeat(begpadding) + string + ' '.repeat(endpadding);
+    return paddedString;
 };
 
 
 
 _.has = (obj, key) => {
-  let hasValue = obj[key] !== undefined;
-  return hasValue;
+	let hasValue = obj[key] !== undefined;
+    return hasValue;
 };
 
 
 
 _.invert = object => {
-  const invertedObject = {};
-  let value = {};
-  for (let key in object) {
-    let originalValue = key.value;
-    invertedObject.key = originalValue;
-    invertedObject.originalValue = key;
-  }
-  return invertedObject;
+	const invertedObject = {};
+    let value = {};
+    for (let key in object) {
+    	let originalValue = key.value;
+    	invertedObject.key = originalValue;
+    	invertedObject.originalValue = key;
+    }
+    return invertedObject;
 };
 
 
 
 _.findKey = (object, predicate) => {
-  for (let key in object) {
-    let value = object[key];
-    let predicateReturnValue = predicate(value);
-    if (predicateReturnValue) {
-      return key;
-    }
-   return undefined;
- }
+	for (let key in object) {
+    	let value = object[key];
+    	let predicateReturnValue = predicate(value);
+    	if (predicateReturnValue) {
+      		return key;
+    	}
+    	return undefined;
+   }
 };
 
 
 
 _.drop = (array, n) => {
-  if (!n) {
-    n = 1;
-  }
-  let droppedArray = array.slice(n);
-  return droppedArray;
+	if (!n) {
+    	n = 1;
+    }
+    let droppedArray = array.slice(n);
+    return droppedArray;
 };
 
 
 
 _.dropWhile = (array, predicate) => {
-  let dropNumber = array.findIndex((element, index) => !predicate(element, index, array));
-  let droppedArray = _.drop(array, dropNumber);
-  return droppedArray;
+	let dropNumber = array.findIndex((element, index) => !predicate(element, index, array));
+    let droppedArray = _.drop(array, dropNumber);
+    return droppedArray;
 };
 
 
 
 _.chunk = (array, size) => {
-  if (!size) {
-    size = 1;
-  }
-  let arrayChunks = [];
-  for (let i = 0; i < array.length; i+size) {
-    let arrayChunk = array.slice(i, i += size);
-    arrayChunks.push(arrayChunk);
-  }
-  return arrayChunks;
-} ;
+	if (!size) {
+    	size = 1;
+    }
+    let arrayChunks = [];
+    for (let i = 0; i < array.length; i+size) {
+    	let arrayChunk = array.slice(i, i += size);
+    	arrayChunks.push(arrayChunk);
+    }
+    return arrayChunks;
+};
 
 
 
